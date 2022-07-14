85a085ee53a3604e62976401022290245c7c2004
vilchesplus <vilchesplus@gmail.com>
2022-07-14 09:23:07 +0200

    Cambios en script, saca más in formación...

--- telegram-web-scraper-bot.py
+++ telegram-web-scraper-bot.py
@@ -8,6 +8,11 @@ import smtplib,time, hashlib
 import requests
 import urllib
 from decouple import config
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 TOKEN = config('TOKEN')
 
 #Read the website and read time interval
@@ -15,7 +20,7 @@ input_website = 'https://analisiscalidadaire.madrid.es/situacionactual'
 
 while True:
 #Visit the website to know if it is up
-#Visit the website to know if it is up
+
     status = urllib.request.urlopen(input_website).getcode()
 #If it returns 200, the website is up
     if status != 200:
@@ -25,9 +30,18 @@ while True:
         requests.get("https://api.telegram.org/bot" + TOKEN + "/sendMessage?chat_id=5002532208&text={}".format(ResultText))
     else:
         ResultText = "The website is up"
- 
+        options = Options()
+        options.headless = True
+        driver = webdriver.Remote("http://127.0.0.1:4444/wd/hub",options=options)
+       # driver = webdriver.Chrome('C:/Users/rvilchef/OneDrive - NTT DATA EMEAL/chromedriver', options=options)
+        driver.get("https://analisiscalidadaire.madrid.es/situacionactual")
+        time.sleep(5)
+        a = driver.find_element(by=By.XPATH, value=("//*[@id='tiempo_real_fecha']")).text
+        time.sleep(5)
+
         print(ResultText)
-        requests.get("https://api.telegram.org/bot" + TOKEN + "/sendMessage?chat_id=5002532208&text={}".format(ResultText))
+        b = "Última carga de datos realizada: {}, \n {}".format(a, ResultText)
+        requests.get("https://api.telegram.org/bot" + TOKEN + "/sendMessage?chat_id=5002532208&text={}".format(b))
         time.sleep(2000)
 
 
