9097ee443086da276ddc4a96392f3e4474a7a272
vilchesplus <68906294+vilchesplus@users.noreply.github.com>
2022-07-21 10:54:41 +0200

    Update telegram-web-scraper-bot.py

--- telegram-web-scraper-bot.py
+++ telegram-web-scraper-bot.py
@@ -58,7 +58,7 @@ while True:
         ResultText = "The website is up"
         options = Options()
         options.headless = True
-        driver = webdriver.Remote("http://192.168.8.51:4444/wd/hub",options=options)
+        driver = webdriver.Remote("http://192.168.90.48:4444/wd/hub",options=options)
         #driver = webdriver.Chrome('C:/Users/rvilchef/OneDrive - NTT DATA EMEAL/chromedriver', options=options)
         driver.get("https://analisiscalidadaire.madrid.es/situacionactual")
         time.sleep(5)
